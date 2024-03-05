b414c7b265c2b58bb94e1aaedb139d2caa8384f9
Miha0809 <miha080104@gmail.com>
2024-03-05 23:04:56 +0100

    Delete company
    Took 7 minutes

--- WestServiceIf/Controllers/CompanyController.cs
+++ WestServiceIf/Controllers/CompanyController.cs
@@ -1,5 +1,9 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WestServiceIf.Models;
 using WestServiceIf.Services;
 
 namespace WestServiceIf.Controllers;
@@ -10,5 +14,28 @@ public class CompanyController(WSIDbContext context) : Controller
     public async Task<IActionResult> Companies()
     {
         return View(await context.Companies.ToListAsync());
+    }
+    
+    [Authorize]
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        var company = await context.Companies.FindAsync(id);
+        
+        if (id is null || company is null)
+        {
+            return Error();
+        }
+                
+        context.Companies.Remove(company);
+        context.SaveChanges();
+
+        return RedirectToAction("Companies");
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
