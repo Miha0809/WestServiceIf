8c80a5d4bfcb1edcb2945d678982020ae434265d
Mark Full <40777154+Miha0809@users.noreply.github.com>
2024-03-05 19:02:29 +0100

    Auth (#2)
    * Login
    
    * Logout
    
    Took 7 minutes

---
+++ WestServiceIf/Controllers/AuthorizationController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WestServiceIf.Models;
+using WestServiceIf.Models.DTOs;
+
+namespace WestServiceIf.Controllers;
+
+public class AuthorizationController(SignInManager<IdentityUser> signInManager) : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginDto login)
+    {
+        var result = await signInManager.PasswordSignInAsync(login.Email, login.Password, isPersistent: false, lockoutOnFailure: false);
+        
+        if (result.Succeeded)
+        {
+            return RedirectToAction(); // TODO: Redirect
+        }
+
+        return RedirectToAction("Error");
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        Response.Cookies.Delete(".AspNetCore.Identity.Application");
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
